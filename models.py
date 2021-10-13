eb302407eb707bc16832bebc3347ada7b729c08c
christina lucia <89708073+Xtinalucia@users.noreply.github.com>
2021-10-13 01:41:09 -0500

    Update models.py

--- models.py
+++ models.py
@@ -7,9 +7,9 @@ from werkzeug.security import generate_password_hash, check_password_hash
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), nullable=False, unique=True)
-    email = db.Column(db.String(150), nullable=False, unique=True)
+    phonenumber = db.Column(db.String(150), nullable=False, unique=True)
     password = db.Column(db.String(256), nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True)
+    
 
     def __init__(self, username, phonenumber, password):
         self.username=username
